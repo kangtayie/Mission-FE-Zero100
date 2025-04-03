b70226fa905d7603c749576181e2386879c23623
sispo3314 <127939853+sispo3314@users.noreply.github.com>
2025-04-03 21:45:12 +0900

    Revert "[2주차] 김지민/ TodoList 스타일 적용, 컴포넌트 모듈화"

--- src/component/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ children, onClick, className = '', ...props }) => {
-  return (
-    <button onClick={onClick} className={className} {...props}>
-      {children}
-    </button>
-  )
-}
-
-export default Button
