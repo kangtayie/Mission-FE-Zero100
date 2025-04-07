c8787e944d2ae713b69427dc87c91e533f8a1ec4
tayiekang <taei518@naver.com>
2025-04-07 11:33:33 +0900

    2주차과제수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
 import styled from "styled-components";
-import Text from "./component/Text";
-import Input from "./component/Input";
-import Button from "./component/Button";
-import Checkbox from "./component/Checkbox";
-
+import Header from "./component/Header";
+import AddTodo from "./component/AddTodo";
+import Category from "./component/Category";
+import TodoList from "./component/TodoList";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  // width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f9f9f9;
+
+  @media (max-width: 768px) and (orientation: portrait) {
+    max-width: 90%;
+    padding: 1.5rem;
+  }
+
+  @media (max-width: 480px) and (orientation: portrait) {
+    padding: 1rem;
+  }
 `;
 
 function App() {
   return (
-    <div className="app-container">
-      {/* text part */}
-      <Text />
-
-      {/* input part */}
-      <Input />
-     {/* button part */}
-      <Button />
-      {/* checkbox part */}
-      <Checkbox />
-    </div>
+    <Container>
+      {/* Header part */}
+      <Header />
+      {/* AddTodo part */}
+      <AddTodo />
+      {/* AddTodo part */}
+      <Category />
+      {/* TodoList part */}
+      <TodoList />
+    </Container>
   );
-}
+};
 
 export default App;
